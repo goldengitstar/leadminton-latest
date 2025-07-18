8974b8692b3e988e72f07a0faa3ce714b140e4fa
egan <nzukiegan6@gmail.com>
2025-07-18 18:11:35 +0300

    updates

--- src/pages/InterclubPage.tsx
+++ src/pages/InterclubPage.tsx
@@ -649,7 +649,7 @@ const InterclubPage: React.FC = () => {
                         className={`px-4 py-2 rounded-lg flex items-center space-x-2 ${
                           showLineupBuilder 
                             ? 'bg-gray-200 text-gray-800' 
-                            : 'bg-blue-600 text-white hover:bg-purple-700'
+                            : 'bg-purple-700 text-white'
                         }`}
                       >
                         <Settings className="w-4 h-4" />
