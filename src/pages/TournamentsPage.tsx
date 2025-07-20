772fc61af7c923f490c8112de22277163aa925e2
egan <nzukiegan6@gmail.com>
2025-07-20 08:04:01 +0300

    updates

--- src/pages/TournamentsPage.tsx
+++ src/pages/TournamentsPage.tsx
@@ -132,8 +132,7 @@ export default function TournamentsPage() {
       loadedTournaments?.forEach(tournament =>
         tournament.rounds.forEach(round =>
           round.matches.forEach(match => {
-            console.log("player 1", playerMap.get(String(match.player1Id)));
-            match.players = [playerMap.get(String(match.player1_id)), playerMap.get((match.player2Id))].filter(Boolean);
+            match.players = [playerMap.get(String(match.player1Id)), playerMap.get((match.player2Id))].filter(Boolean);
           })
         )
       );
