c056906fea88c12bf22ab64eb3bc11044b840e7f
egan <nzukiegan6@gmail.com>
2025-08-03 17:35:41 +0300

    updates

--- src/pages/admin/AdminCpuTeams.tsx
+++ src/pages/admin/AdminCpuTeams.tsx
@@ -1528,6 +1528,7 @@ const AdminCpuTeams: React.FC<AdminCpuTeamsProps> = () => {
                     </label>
                     <input
                       type="number"
+                      disabled
                       value={playerFormData.level || 1}
                       onChange={(e) => setPlayerFormData(prev => ({ ...prev, level: parseInt(e.target.value) }))}
                       className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
